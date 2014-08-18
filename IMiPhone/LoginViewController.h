6f2e2c2712845fa0272a332ce6d4856a5b4b6048
yinowl <yinowl@hotmail.com>
2014-08-18 15:29:42 +0800

    +add login and register

---
+++ IMiPhone/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14-8-18.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ IMiPhone/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14-8-18.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
