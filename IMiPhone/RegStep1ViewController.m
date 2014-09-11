f1632fb4a2bf92117c6f92f78ca8d6afc28f2bbf
GuoliangWang <wgl413@sina.com>
2014-09-11 17:34:00 +0800

    + 发送验证码倒计时

--- IMiPhone/RegStep1ViewController.m
+++ IMiPhone/RegStep1ViewController.m
@@ -27,6 +27,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self setBtnSendTitle:NSLocalizedString(@"Send",@"发送")];
+    btnState = btnStateSend;
 }
 
 - (void)didReceiveMemoryWarning
@@ -45,5 +47,74 @@
     // Pass the selected object to the new view controller.
 }
 */
+#define BTN_STATE_SEND 0
+enum{
+    btnStateSend = 0,
+    btnStateWaiting,
+    btnStateResend
+    
+};
+int totalTime = 30;
+int remainTime;
+int btnState;
+NSTimer *nextSendTimer;
+
+- (IBAction)onTouchUpInsideBtnSend:(id)sender {
+    
+    UIButton *btnSend = (UIButton *)[self.view viewWithTag:2];
+    switch (btnState) {
+        case btnStateSend:
+        case btnStateResend:
+            if([self codeIsValid])
+            {
+                [self setBtnSendTitle:@""];
+//              [btnSend setTitle:@"" forState:UIControlStateNormal];
+                [self startNextSendTimer];
+                NSLog(@"send check code msg to server!!!");
+            }
+            btnState = btnStateWaiting;
+            btnSend.enabled = NO;
+            break;
+        case btnStateWaiting:
+            break;
+        default:
+            break;
+    }
+   
+    
+}
+-(void) setBtnSendTitle:(NSString *)value{
+    UIButton *btnSend = (UIButton *)[self.view viewWithTag:2];
+    [btnSend setTitle:value forState:UIControlStateNormal];
+    [btnSend setTitle:value forState:UIControlStateDisabled];
+    [btnSend setTitle:value forState:UIControlStateSelected];
+
+
+}
+-(BOOL) codeIsValid{
+    NSLog(@"codeIsValid");
+    return YES;
+}
+- (void) startNextSendTimer{
+    remainTime = totalTime;
+    [self setBtnSendTitle:[NSString stringWithFormat:@"%d",remainTime]];
+    if(nextSendTimer == nil)
+        nextSendTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(nextSendTimerFire:) userInfo:nil repeats:YES];
+}
+-(void)nextSendTimerFire:(NSTimer *)theTimer
+{
+    remainTime --;
+    [self setBtnSendTitle:[NSString stringWithFormat:@"%d",remainTime]];
+    if(remainTime <= 0)
+    {
+        UIButton *btnSend = (UIButton *)[self.view viewWithTag:2];
+        btnSend.enabled = YES;
+        btnState = btnStateResend;
+        [self setBtnSendTitle:NSLocalizedString(@"Resend",@"重新发送")];
+        [nextSendTimer invalidate];
+        nextSendTimer = nil;
+
+    }
+}
 
 @end
