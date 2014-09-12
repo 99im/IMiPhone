74eb0ff46914ac3c935f31ca89e19bf1875f7d61
yinowl <yinowl@hotmail.com>
2014-09-12 16:47:30 +0800

    + 添加协议定义工具

--- scripts/exportProtoDefine.sh
+++ scripts/exportProtoDefine.sh
@@ -6,5 +6,5 @@
 #  Created by 尹晓君 on 14-9-11.
 #  Copyright (c) 2014年 尹晓君. All rights reserved.
 cd $( dirname ${0} )
-java -jar ../../tools/com.jascava.tools_v1.2.0.v20140911.jar ExportProtoDefine -h ./MessageDefine.pch -url "http://local.admin.taiqiu.com:7070/tool/proto.php?action=get&prid=1&kind=3&verify=fef5f949928796a4bf733e494c5008dc"
+java -jar ../tools/com.jascava.tools_v1.2.0.v20140911.jar ExportProtoDefine -h ./MessageDefine.pch -url "http://local.admin.taiqiu.com:7070/tool/proto.php?action=get&prid=1&kind=3&verify=fef5f949928796a4bf733e494c5008dc"
 cp ./MessageDefine.pch ../IMiPhone/
