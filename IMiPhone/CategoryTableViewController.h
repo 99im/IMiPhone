30c439ddcec2ae5f82053c27c789bda6c03db811
yinowl <yinowl@hotmail.com>
2014-09-28 19:04:41 +0800

    + 添加联系人分组界面

---
+++ IMiPhone/CategoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoryTableViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14-9-28.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryTableViewController : UITableViewController
+
+@end
