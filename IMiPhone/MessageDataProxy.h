3df7b832f9a878b2876c8754f2e9ff808ff616d4
GuoliangWang <wgl413@sina.com>
2014-09-30 16:42:15 +0800

    * 临时

--- IMiPhone/MessageDataProxy.h
+++ IMiPhone/MessageDataProxy.h
@@ -6,18 +6,13 @@
 //  Copyright (c) 2014年 尹晓君. All rights reserved.
 //
 
-#import "MessageDAO.h"
-#import "MessageGroupDAO.h"
-#import "DPMessage.h"
-#import "DPMessageGroup.h"
-#import "ImDataUtil.h"
+#import <Foundation/Foundation.h>
 
 @interface MessageDataProxy : NSObject
 
-+ (MessageDataProxy*)sharedProxy;
+@property (nonatomic, retain) NSMutableArray *arrMessages;
+@property (nonatomic, retain) NSMutableArray *arrMessageGroups;
 
-//修改数组中元素内容，需用数组replace操作，以触发observer的相关逻辑
-- (NSMutableArray *)mutableArrayMessages;
-- (NSMutableArray *)mutableArrayMessageGroups;
++ (MessageDataProxy*)sharedProxy;
 
 @end
