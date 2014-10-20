95cd8a39b19b09c33e3df39cd3dc9045b7c8ca27
雷运梁 <lei@leiyunliangdeMac-mini.local>
2014-10-20 14:07:43 +0800

    + GroupDataProxy.h / .m + GroupMessageProxy.h / .m

---
+++ IMiPhone/GroupMessageProxy.h
@@ -0,0 +1,13 @@
+//
+//  GroupMessageProxy.h
+//  IMiPhone
+//
+//  Created by 雷运梁 on 14-10-20.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GroupMessageProxy : NSObject
+
+@end
