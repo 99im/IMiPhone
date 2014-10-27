58a1998abf0a6d9bba093c42cb4c6104a0fe1ff0
yinowl <yinowl@hotmail.com>
2014-10-27 16:10:59 +0800

    + 添加丢失SystemTableView文件，添加群聊界面

---
+++ IMiPhone/GroupChatViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupChatViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14/10/27.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupChatViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ IMiPhone/SystemTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SystemTableViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14-9-25.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SystemTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *cellMyInfo;
+
+@end
