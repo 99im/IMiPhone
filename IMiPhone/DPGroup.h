f628cf43b1106c7ca775c319a8040e759a828846
雷运梁 <lei@leiyunliangdeMac-mini.local>
2014-10-29 14:12:20 +0800

    * 取消：DPGroup结构继承自 DBGroup

--- IMiPhone/DPGroup.h
+++ IMiPhone/DPGroup.h
@@ -7,8 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "DBGroup.h"
 
-@interface DPGroup : DBGroup
+#define DB_PRIMARY_KEY_GROUP_ID @"groupId"
+
+@interface DPGroup : NSObject
+#pragma mark - 群基信息
+@property (nonatomic) long  gid;
+@property (nonatomic, retain) NSString *name;
+@property (nonatomic) NSInteger memberNum;  //成员数
+@property (nonatomic, retain) NSString *ctime;
+@property (nonatomic, retain) NSString *intro;
+@property (nonatomic, retain) NSString *portraitUrl;//头像地址
+
+#pragma mark - 群主信息
+@property (nonatomic) long creator_uid;
+@property (nonatomic, retain) NSString *creator_nick;
+@property (nonatomic) long creator_oid;
+@property (nonatomic) long creator_vip;
+@property (nonatomic, retain) NSString *creator_city;
+
+#pragma mark - 客户端附加信息
+@property (nonatomic) long long localUpdateTime;  //本地存储更新时间，格式：2014110241259
 
 @end
--- IMiPhone/DPGroup.m
+++ IMiPhone/DPGroup.m
@@ -1,5 +1,5 @@
 //
-//  DPGroup.m
+//  Group.m
 //  IMiPhone
 //
 //  Created by 雷运梁 on 14-10-22.
@@ -9,5 +9,20 @@
 #import "DPGroup.h"
 
 @implementation DPGroup
+
+@synthesize gid;
+@synthesize name;
+@synthesize memberNum;
+@synthesize ctime;
+@synthesize intro;
+@synthesize portraitUrl;
+
+@synthesize creator_uid;
+@synthesize creator_nick;
+@synthesize creator_oid;
+@synthesize creator_vip;
+@synthesize creator_city;
+
+@synthesize localUpdateTime;
 
 @end
