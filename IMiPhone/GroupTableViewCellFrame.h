def6e3fb3b9958ed80fa8e4feb3ba465e7bc031a
GuoliangWang <wgl413@sina.com>
2014-11-01 12:12:15 +0800

    修改群聊

---
+++ IMiPhone/GroupTableViewCellFrame.h
@@ -0,0 +1,13 @@
+//
+//  GroupTableViewCellFrame.h
+//  IMiPhone
+//
+//  Created by 王 国良 on 14/11/1.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import "ChatTableViewCellFrame.h"
+
+@interface GroupTableViewCellFrame : ChatTableViewCellFrame
+
+@end
