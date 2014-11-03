bd5931597f9089f586a356fc6b9b462cd6940509
雷运梁 <lei@leiyunliangdeMac-mini.local>
2014-11-03 14:28:53 +0800

    调整群消息文字等

--- IMiPhone/MsgGroupApplyTableViewCell.m
+++ IMiPhone/MsgGroupApplyTableViewCell.m
@@ -14,49 +14,57 @@
 
 @synthesize rid;
 
-- (void)awakeFromNib {
-  // Initialization code
+- (void)awakeFromNib
+{
+    // Initialization code
 }
 
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-  [super setSelected:selected animated:animated];
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
 
-  // Configure the view for the selected state
+    // Configure the view for the selected state
 }
 
-- (void)drawCellBody:(DPSysMessageGroup *)dpSysMsg {
-    //self.lblTitle.text = dpSysMsg.title;
-    //self.lblContent.text = dpSysMsg.content;
-    //NSLog(@"加群消息：\nmodid:%i\ntype:%i\ntargetId:%qi\ngroupName:%@" , dpSysMsg.modid , dpSysMsg.type , dpSysMsg.rid , dpSysMsg.groupName);
+- (void)drawCellBody:(DPSysMessageGroup *)dpSysMsg
+{
+    // self.lblTitle.text = dpSysMsg.title;
+    // self.lblContent.text = dpSysMsg.content;
+    // NSLog(@"加群消息：\nmodid:%i\ntype:%i\ntargetId:%qi\ngroupName:%@" , dpSysMsg.modid , dpSysMsg.type ,
+    // dpSysMsg.rid , dpSysMsg.groupName);
 
     //临时处理
-    //NSLog(@"加群消息：type:%i" , MSG_GROUP_APPLY);
+    // NSLog(@"加群消息：type:%i" , MSG_GROUP_APPLY);
     NSInteger type = dpSysMsg.type;
-    if (dpSysMsg.type == GROUP_MSG_TYPE_APPLY) {//申请入群
+    self.btnAgree.hidden = YES;
+    if (dpSysMsg.type == GROUP_MSG_TYPE_APPLY) { //申请入群
         self.rid = dpSysMsg.rid;
+        self.lblTitle.text = [NSString stringWithFormat:@"%@", dpSysMsg.ctime];
         if (dpSysMsg.status == GROUP_MSG_STATUS_NEW) {
             self.btnAgree.hidden = NO;
-            self.lblTitle.text = [NSString stringWithFormat:@"加群申请-待处理"];
-        } else {
-            self.btnAgree.hidden = YES;
-            self.lblTitle.text = [NSString stringWithFormat:@"加群申请-已处理"];
+            self.lblContent.text = [NSString
+                stringWithFormat:@"待处理: %@申请加入群【%@】", dpSysMsg.userNick, dpSysMsg.groupName];
         }
-        self.lblContent.text = [NSString stringWithFormat:@"%@:申请加入群【%@】" , dpSysMsg.userNick , dpSysMsg.groupName];
+        else {
+            self.lblContent.text = [NSString
+                stringWithFormat:@"已处理: %@申请加入群【%@】", dpSysMsg.userNick, dpSysMsg.groupName];
+        }
     }
     else if (type == GROUP_MSG_TYPE_APPLY_PROCESSED) {
-        self.lblTitle.text = [NSString stringWithFormat:@"加群申请-已被批准"];
-        self.lblContent.text = [NSString stringWithFormat:@"你已加入群【%@】" , dpSysMsg.groupName];
+        self.lblTitle.text = [NSString stringWithFormat:@"%@", dpSysMsg.ctime];
+        self.lblContent.text = [NSString stringWithFormat:@"你申请加入群【%@】已被批准", dpSysMsg.groupName];
     }
     else {
-        self.btnAgree.hidden = YES;
-        self.lblTitle.text = [NSString stringWithFormat:@"[%i]%@" , dpSysMsg.type , dpSysMsg.title];
+        self.lblTitle.text = [NSString stringWithFormat:@"[%i]%@", dpSysMsg.type, dpSysMsg.title];
         self.lblContent.text = dpSysMsg.content;
     }
 }
 
-- (IBAction)btnAgreeTouchUp:(id)sender {
-  NSLog(@"开始同意加群请求:%qi", self.rid);
-   [[GroupMessageProxy sharedProxy] sendGroupApplyResponse:self.rid
-                                                      agree:1];
+- (IBAction)btnAgreeTouchUp:(id)sender
+{
+    if (self.rid > 0) {
+        NSLog(@"开始同意加群请求:%qi", self.rid);
+        [[GroupMessageProxy sharedProxy] sendGroupApplyResponse:self.rid agree:1];
+    }
 }
 @end
