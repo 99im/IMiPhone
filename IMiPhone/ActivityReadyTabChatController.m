8d18f71e685874d59e6240de4cbd6bb83404fccf
GuoliangWang <wgl413@sina.com>
2014-11-05 18:30:29 +0800

    添加活动相关类

---
+++ IMiPhone/ActivityReadyTabChatController.m
@@ -0,0 +1,13 @@
+//
+//  ActivityReadyTabChatController.m
+//  IMiPhone
+//
+//  Created by 王 国良 on 14/11/5.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import "ActivityReadyTabChatController.h"
+
+@implementation ActivityReadyTabChatController
+
+@end
---
+++ IMiPhone/ActivityRunningTabChatController.m
@@ -0,0 +1,13 @@
+//
+//  ActivityRunningTabChatController.m
+//  IMiPhone
+//
+//  Created by 王 国良 on 14/11/5.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import "ActivityRunningTabChatController.h"
+
+@implementation ActivityRunningTabChatController
+
+@end
