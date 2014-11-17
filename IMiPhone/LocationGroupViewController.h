77e2dfb8a2f83a6756115694ca86a21e95b918cb
雷运梁 <lei@leiyunliangdeMac-mini.local>
2014-11-17 16:23:43 +0800

    群选择地点测试

---
+++ IMiPhone/LocationGroupViewController.h
@@ -0,0 +1,14 @@
+//
+//  LocationGroupViewController.h
+//  IMiPhone
+//
+//  Created by 雷运梁 on 14-11-17.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import "LocationViewController.h"
+
+@interface LocationGroupViewController : LocationViewController <CLLocationManagerDelegate>
+- (IBAction)goBackTouchUp:(id)sender;
+
+@end
--- IMiPhone/LocationViewController.m
+++ IMiPhone/LocationViewController.m
@@ -9,20 +9,23 @@
 #import "LocationViewController.h"
 
 @interface LocationViewController ()
-
+@property(nonatomic, strong) CLLocationManager *locationManager;
 @end
 
 @implementation LocationViewController
 
+@synthesize locationManager = _locationManager;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 /*
 #pragma mark - Navigation
@@ -33,5 +36,42 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+//
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"location error:%@", error);
+}
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *currLocation = [locations lastObject];
+    NSLog(@"纬度:%3.5f 经度:%3.5f" , currLocation.coordinate.latitude , currLocation.coordinate.longitude);
+
+}
+
+#pragma mark - Location
+-(void) startUpdatingLocation:(id)delegate {
+    // 开始定位
+    if ([ CLLocationManager locationServicesEnabled]) {
+        // 初始化定位管理对象
+        if (_locationManager == nil) {
+            _locationManager = [[CLLocationManager alloc] init];
+            _locationManager.delegate = delegate;
+            _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters; //10米级
+            _locationManager.distanceFilter = 100.0f;
+        }
+        NSLog(@"开始定位:");
+        [_locationManager startUpdatingLocation];
+    } else {
+        NSLog(@"请开启定位功能。");
+    }
+}
+
+-(void) stopUpdatingLocation {
+    // 停止定位
+    NSLog(@"停止定位:");
+   [_locationManager stopUpdatingLocation];
+
+}
 
 @end
