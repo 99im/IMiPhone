e4ce401b99dddc6b0eec62c3c491130c2eee5c1c
yinowl <yinowl@hotmail.com>
2014-11-18 10:25:52 +0800

    Revert "删除 不用文件"
    This reverts commit 1b397da555ae44ce72ff89830c9246de81484825.

---
+++ AccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountViewController.h
+//  IMiPhone
+//
+//  Created by 尹晓君 on 14/11/11.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountViewController : UIViewController
+
+@end
