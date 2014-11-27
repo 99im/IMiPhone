e29bb79ed84042a56632c8e9c4fd20aca2f2158f
GuoliangWang <wgl413@sina.com>
2014-11-28 00:01:30 +0800

    修改活动

---
+++ IMiPhone/ActivityInfoApplyCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityInfoApplyCell.h
+//  IMiPhone
+//
+//  Created by 王 国良 on 14/11/27.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityInfoApplyCell : UITableViewCell
+
+@end
---
+++ IMiPhone/ActivityInfoTitleCell.m
@@ -0,0 +1,23 @@
+//
+//  ActivityInfoTitleCell.m
+//  IMiPhone
+//
+//  Created by 王 国良 on 14/11/27.
+//  Copyright (c) 2014年 尹晓君. All rights reserved.
+//
+
+#import "ActivityInfoTitleCell.h"
+
+@implementation ActivityInfoTitleCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
